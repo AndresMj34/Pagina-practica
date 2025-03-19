089a0aa182f2aea36412a8a66eaa841ad87f7cf5
AndresMj34 <luis.majeae@gmail.com>
2025-03-19 18:50:28 -0500

    Pagina paractica

---
+++ clase-6/js/main.js
@@ -0,0 +1,12 @@
+
+let nota=  console.log ("la nota es de 5");
+
+while (nota!=0) {
+    let contnota=1;
+
+    let valor= nota/contnota;
+
+    console.log("la nota es: "+valor);
+    contnota++;
+}
+
---
+++ clase-Js/js/mian.js
@@ -0,0 +1,42 @@
+//alert("Hola mundo")
+
+
+//let nombre=prompt("Ingrese su nombre");//JOptionPane entrada input
+
+//alert("Hola mundo");  JOtionPane.showMessageDialog()
+//console.log ("Hola mundo"+nombre); // Sout
+
+//let edad= prompt("ingrese la edad");
+
+//let edad2= prompt("ingrese la edad");
+
+//if (edad<edad2) {
+  //  console.log("la peona 2 es mayor");
+//    alert("La persona 2 es mayir");
+//}else{
+//alert("La persona 1 es mayir");
+//console.log("la persona 1 es mayor ")
+//}
+
+let nm = prompt("ingrese numero");
+for (let index = 0; index <= nm; index++) {
+    console.log("nmum "+index);
+
+    if(nm===4){
+    
+continue;
+
+    }
+
+    
+    
+}
+
+
+
+const array1=["uno, dos, tres"];
+const array01=["nueve, ocho, cinco"];
+
+
+
+
